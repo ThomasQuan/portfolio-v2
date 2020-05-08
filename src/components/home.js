39296916fb419aa853c607a2dbf2970f4f09ab5f
Thomas Quan <quantrithanh1999@gmail.com>
2020-05-08 02:46:09 -0400

    formal greeting done "

--- src/components/home.js
+++ src/components/home.js
@@ -3,30 +3,36 @@ import $ from "jquery";
 class Home extends Component {
   state = {};
   openSlide = () => {
-    $([document.documentElement, document.body]).animate({
-      scrollTop: $("#about-me").offset().top
-  }, 1500);
+    $([document.documentElement, document.body]).animate(
+      {
+        scrollTop: $("#about-me").offset().top,
+      },
+      1500
+    );
+    console.log("Clicked")
   };
   render() {
     return (
       <React.Fragment>
         <div className="container" data-aos="fade-out" data-aos-duration="3000">
-          <h1 className="container-title">Welcome to my portfolio</h1>
-
+          {/* <h1 className="container-title">Welcome to my portfolio</h1> */}
+          <div className="outer-layer">
+            <div class="clip-text">Thomas.Q</div>
+          </div>
           <button onClick={this.openSlide} id="sliderToggle">
-            <input type="checkbox" />
+              <input type="checkbox" />
 
-            {/* Left */}
-            <div className="left-wing">
-              <span></span>
-              <span></span>
-            </div>
-            {/* Right */}
-            <div className="right-wing">
-              <span></span>
-              <span></span>
-            </div>
-          </button>
+              {/* Left */}
+              <div className="left-wing">
+                <span></span>
+                <span></span>
+              </div>
+              {/* Right */}
+              <div className="right-wing">
+                <span></span>
+                <span></span>
+              </div>
+            </button>
         </div>
       </React.Fragment>
     );
